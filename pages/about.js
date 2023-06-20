000b1dbd1d433c7ffaf9a29635892668df40b02b
Richard Schave <schavester@gmail.com>
2023-06-19 18:24:15 -0700

    H1 tag our map of empty los angeles

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,7 @@ export default function About({ authors, siteconfig }) {
     <Layout {...siteconfig}>
       <Container>
         <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
-          About
+          Our map of Empty Los Angeles
         </h1>
         <div className="text-center">
           <p className="text-lg">Our map of Empty Los Angeles</p>
