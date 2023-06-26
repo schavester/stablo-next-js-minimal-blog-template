e8baf2c4503bc85a2033ab1eb486a431af416da9
Richard Schave <schavester@gmail.com>
2023-06-26 16:50:51 -0700

    added map page

---
+++ pages/map.js
@@ -0,0 +1,46 @@
+import Container from "@components/container";
+import Layout from "@components/layout";
+import { authorsquery, configQuery } from "@lib/groq";
+import { getClient } from "@lib/sanity";
+import GetImage from "@utils/getImage";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Map({ authors, siteconfig }) {
+  return (
+    <Layout {...siteconfig}>
+      <Container>
+        <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
+          Our map of Empty Los Angeles
+        </h1>
+        <div className="text-center">
+          <p className="text-lg">Our map of Empty Los Angeles</p>
+        </div>
+
+       
+
+        <div className="mx-auto prose text-center dark:prose-invert mt-14">
+          <p>
+<iframe src="https://www.google.com/maps/d/embed?mid=1o9HbMpGQU8GDVu-4W5KC-zbUG_gQCao&ehbc=2E312F" width="640" height="480"></iframe>
+          </p>
+          <p> <Link href="/contact">Get in touch</Link>
+          </p>
+        </div>
+      </Container>
+    </Layout>
+  );
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  //console.log(params);
+  const authors = await getClient(preview).fetch(authorsquery);
+  const config = await getClient(preview).fetch(configQuery);
+  return {
+    props: {
+      authors: authors,
+      siteconfig: { ...config },
+      preview
+    },
+    revalidate: 100
+  };
+}
